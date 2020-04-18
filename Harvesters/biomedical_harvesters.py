7a1841c71f0617071c0afbd013881a60df599466
GyftakisK <gyftakiskon@gmail.com>
2020-04-18 14:23:22 +0300

    EntrezWrapper + OBOWrapper

--- Harvesters/biomedical_harvesters.py
+++ Harvesters/biomedical_harvesters.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+import ntpath
+import datetime
 from utilities import run_jar
 
 
@@ -13,37 +15,68 @@ class HarvesterWrapper:
 
     def run(self):
         self._create_settings_file()
-        args = self._get_arguments()
+        args = self._get_jar_arguments()
         output = self._run_jar(args)
-        print(output)
+        self._check_output(output)
         self._clean_settings_file()
 
     def _create_settings_file(self):
-        raise NotImplemented
-
-    def _get_arguments(self):
-        raise NotImplemented
+        settings = self._get_settings()
+        with open(self._settings_file, 'w') as settings_file:
+            yaml.dump(settings, settings_file)
 
     def _run_jar(self, args):
         return run_jar(self._harvester_jar_name, args)
 
     def _clean_settings_file(self):
+        os.remove(self._settings_file)
+
+    def _get_jar_arguments(self):
         raise NotImplemented
+
+    def _get_settings(self):
+        raise NotImplemented
+
+    def _check_output(self, output):
+        pass
 
 
 class HarvestDrugBankWrapper(HarvesterWrapper):
-    def __init__(self, file_path, mongo_host, mongo_host_port, mongo_dbname, mongo_collection):
+    def __init__(self, file_path, mongo_host, mongo_host_port, mongo_dbname, mongo_collection="DrugBank"):
         super().__init__("HarvestDrugBank.jar", mongo_host, mongo_host_port, mongo_dbname)
         self._mongo_config["collection"] = mongo_collection
         self._file_path = file_path
 
-    def _create_settings_file(self):
-        settings = {"inputFilePath": self._file_path, "mongodb": self._mongo_config}
-        with open(self._settings_file, 'w') as settings_file:
-            yaml.dump(settings, settings_file)
+    def _get_settings(self):
+        return {"inputFilePath": self._file_path, "mongodb": self._mongo_config}
 
-    def _get_arguments(self):
+    def _get_jar_arguments(self):
         return self._settings_file
 
-    def _clean_settings_file(self):
-        os.remove(self._settings_file)
+
+class HarvestOBOWrapper(HarvesterWrapper):
+    def __init__(self, file_path, mongo_host, mongo_host_port, mongo_dbname):
+        super().__init__("HarvestOBO.jar", mongo_host, mongo_host_port, mongo_dbname)
+        self._base_folder = os.path.abspath(file_path)
+        self._input_obo_name = ntpath.basename(file_path).strip(".obo")
+
+    def _get_settings(self):
+        return {"baseFolder": self._base_folder, "inputOBOName": self._input_obo_name, "mongodb": self._mongo_config}
+
+    def _get_jar_arguments(self):
+        return self._settings_file
+
+
+class HarvestEntrezWrapper(HarvesterWrapper):
+    def __init__(self, dataset_id, mesh_term, base_dir, last_update,  mongo_host, mongo_host_port, mongo_dbname):
+        super().__init__("HarvestOBO.jar", mongo_host, mongo_host_port, mongo_dbname)
+        self._base_folder = base_dir
+        self._last_update = last_update.strftime("%Y/%m/%d")
+        self._dataset_id = dataset_id
+        self._mesh_term = mesh_term
+
+    def _get_settings(self):
+        return {"baseFolder": self._base_folder, "lastUpdate": self._last_update, "mongodb": self._mongo_config}
+
+    def _get_jar_arguments(self):
+        return self._dataset_id, self._mesh_term, datetime.datetime.now().strftime("%Y/%m/%d")
--- Harvesters/tests/biomedical_harvesters_tests.py
+++ Harvesters/tests/biomedical_harvesters_tests.py
@@ -1,20 +1,55 @@
 import unittest
 import os
+import datetime
 from unittest.mock import MagicMock
-from Harvesters.biomedical_harvesters import HarvestDrugBankWrapper
+from Harvesters.biomedical_harvesters import HarvestDrugBankWrapper, HarvestOBOWrapper, HarvestEntrezWrapper
 
 
 class HarvestersTestSuite(unittest.TestCase):
 
     def test_drugbank(self):
-        harvester = HarvestDrugBankWrapper("/home/gyftakiskon/diploma/files/DrugBank.xml",
+        harvester = HarvestDrugBankWrapper("/test_path/DrugBank.xml",
                                            mongo_host="127.0.0.1",
                                            mongo_host_port=27017,
                                            mongo_dbname="test",
                                            mongo_collection="test_DrugBank")
         harvester._run_jar = MagicMock(return_value="SUCCESS")
+        harvester._clean_settings_file = MagicMock()
         harvester.run()
-        self.assertFalse(os.path.exists(harvester._settings_file))
+        self.assertTrue(os.path.exists(harvester._settings_file))
+        with open(harvester._settings_file, 'r') as f:
+            print(f.read())
+        os.remove(harvester._settings_file)
+
+    def test_obo(self):
+        harvester = HarvestOBOWrapper("/test_path/doid.obo",
+                                      mongo_host="127.0.0.1",
+                                      mongo_host_port=27017,
+                                      mongo_dbname="test")
+        harvester._run_jar = MagicMock(return_value="SUCCESS")
+        harvester._clean_settings_file = MagicMock()
+        harvester.run()
+        self.assertTrue(os.path.exists(harvester._settings_file))
+        with open(harvester._settings_file, 'r') as f:
+            print(f.read())
+        os.remove(harvester._settings_file)
+
+    def test_entrez(self):
+        harvester = HarvestEntrezWrapper("DMD",
+                                         "Muscular Dystrophy, Duchenne",
+                                         "/test_path/",
+                                         datetime.date(2020, 4, 15),
+                                         mongo_host="127.0.0.1",
+                                         mongo_host_port=27017,
+                                         mongo_dbname="test")
+        harvester._run_jar = MagicMock(return_value="SUCCESS")
+        harvester._clean_settings_file = MagicMock()
+        harvester.run()
+        print(harvester._run_jar.call_args)
+        self.assertTrue(os.path.exists(harvester._settings_file))
+        with open(harvester._settings_file, 'r') as f:
+            print(f.read())
+        os.remove(harvester._settings_file)
 
 
 if __name__ == '__main__':
