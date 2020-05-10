b0a209ac2406dd4c684dd80ce64354bfc35392f5
GyftakisK <gyftakiskon@gmail.com>
2020-05-10 20:30:25 +0300

    Add CLI

--- disease_graph_cli.py
+++ disease_graph_cli.py
@@ -22,24 +22,24 @@ def main():
 
     args = parser.parse_args()
 
-    disease_graph = KnowledgeExtractor()
+    extractor = KnowledgeExtractor()
 
     try:
-        disease_graph.setup()
+        extractor.setup()
         if args.harvest_go:
-            disease_graph.update_obo(args.harvest_go, "GO")
+            extractor.update_obo(args.harvest_go, "GO")
         if args.harvest_do:
-            disease_graph.update_obo(args.harvest_do, "DO")
+            extractor.update_obo(args.harvest_do, "DO")
         if args.harvest_mesh:
-            disease_graph.update_obo(args.harvest_mesh, "MESH")
+            extractor.update_obo(args.harvest_mesh, "MESH")
         if args.harvest_drugbank:
-            disease_graph.update_drugbank(args.harvest_drugbank)
+            extractor.update_drugbank(args.harvest_drugbank)
         if args.add_disease:
-            disease_graph.add_disease(args.add_disease)
+            extractor.add_disease(args.add_disease)
         if args.update_diseases:
-            disease_graph.update_diseases()
+            extractor.update_diseases()
     finally:
-        disease_graph.cleanup()
+        extractor.cleanup()
 
 
 if __name__ == "__main__":
