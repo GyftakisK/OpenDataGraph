bc86898b2cb261aa6382d55f9edbb93fa9db3c41
GyftakisK <gyftakiskon@gmail.com>
2020-05-24 20:47:17 +0300

    Add content to graph options in admin dashboard

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -6,16 +6,16 @@ from flask_wtf.file import FileRequired
 
 class LiteratureForm(FlaskForm):
     mesh_term = StringField('MeSH Term', validators=[DataRequired()])
-    submit = SubmitField('Add to graph')
+    submit1 = SubmitField('Add to graph')
 
 
 class StructuredDrugbankForm(FlaskForm):
     xml_file = FileField(validators=[FileRequired()])
-    submit1 = SubmitField('Add to graph')
+    submit2 = SubmitField('Add to graph')
 
 
 class StructuredOboForm(FlaskForm):
     obo_file = FileField(validators=[FileRequired()])
     obo_type = SelectField('OBO type', choices=(('do', 'DO'), ('go', 'GO'), ('mesh', 'MeSH')),
                            validators=[DataRequired()])
-    submit2 = SubmitField('Add to graph')
+    submit3 = SubmitField('Add to graph')
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,46 +1,47 @@
 from app.admin import bp
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, jsonify
 from app.admin.forms import LiteratureForm, StructuredDrugbankForm, StructuredOboForm
 from flask_login import login_required
 from app import extractor
 
 
-@bp.route('/')
-@bp.route('/dashboard')
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def admin():
     literature_status = extractor.get_literature_status()
     structured_status = extractor.get_structured_resources_jobs_status()
+    form_literature = LiteratureForm()
+    if form_literature.submit1.data and form_literature.validate():
+        flash('Job created for "{}"'.format(form_literature.mesh_term.data))
+        return redirect(url_for("admin.admin"))
+
+    form_drugbank = StructuredDrugbankForm()
+    if form_drugbank.submit2.data and form_drugbank.validate():
+        f = form_drugbank.xml_file.data
+        flash('Job created for "{}"'.format(f.filename))
+        return redirect(url_for("admin.admin"))
+
+    form_obo = StructuredOboForm()
+    if form_obo.submit3.data and form_obo.validate():
+        f = form_obo.obo_file.data
+        flash('Job created for "{}"'.format(f.filename))
+        return redirect(url_for("admin.admin"))
+
     return render_template('admin/dashboard.html',
                            title="Admin dashboard",
                            mesh_terms=literature_status["mesh_terms"],
                            last_update=literature_status["last_update"],
-                           structured_resources=structured_status)
-
-
-@bp.route('literature', methods=['GET', 'POST'])
-@login_required
-def literature():
-    form = LiteratureForm()
-    if form.validate_on_submit():
-        flash('Job created for "{}"'.format(form.mesh_term.data))
-        return redirect(url_for("admin.literature"))
-    return render_template('admin/literature.html', form=form)
-
-
-@bp.route('structured', methods=['GET', 'POST'])
-@login_required
-def structured():
-    form_drugbank = StructuredDrugbankForm()
-    form_obo = StructuredOboForm()
-    if form_drugbank.submit1.data and form_drugbank.validate():
-        f = form_drugbank.xml_file.data
-        flash('Job created for "{}"'.format(f.filename))
-        return redirect(url_for("admin.structured"))
-    if form_obo.submit2.data and form_obo.validate():
-        f = form_obo.obo_file.data
-        flash('Job created for "{}"'.format(f.filename))
-        return redirect(url_for("admin.structured"))
-    return render_template('admin/structured.html',
+                           structured_resources=structured_status,
+                           form_literature=form_literature,
                            form_drugbank=form_drugbank,
                            form_obo=form_obo)
+
+
+@bp.route('update_literature', methods=['POST'])
+@login_required
+def update_literature():
+    import time
+    time.sleep(5)
+    return jsonify({'message': 'Job created for literature update'})
+
