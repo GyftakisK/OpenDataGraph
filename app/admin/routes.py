2a5e33708d5a3e20bd8a32b0015c2405ec81edd6
GyftakisK <gyftakiskon@gmail.com>
2020-05-30 17:21:30 +0300

    Duplicate resources checks

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -16,28 +16,43 @@ def admin():
     structured_status = extractor.get_structured_resources_jobs_status()
     form_literature = LiteratureForm()
     if form_literature.submit1.data and form_literature.validate():
-        task = add_disease_task.apply_async([form_literature.mesh_term.data])
-        flash('Job {} created for "{}"'.format(task.task_id, form_literature.mesh_term.data))
+        mesh_term = form_literature.mesh_term.data
+        if mesh_term in literature_status["mesh_terms"]:
+            flash('Disease with MeSH term {} already in graph'.format(mesh_term))
+            return redirect(url_for("admin.admin"))
+        task = add_disease_task.apply_async([mesh_term])
+        flash('Job {} created for "{}"'.format(task.task_id, mesh_term))
         return redirect(url_for("admin.admin"))
 
     form_drugbank = StructuredDrugbankForm()
     if form_drugbank.submit2.data and form_drugbank.validate():
         f = form_drugbank.xml_file.data
         filename = secure_filename(f.filename)
+        version = form_drugbank.version.data
+        if version in [resource["version"] for resource in structured_status
+                       if resource["type"] == "DRUGBANK"]:
+            flash('Drugbank XML with version {} already in graph'.format(version))
+            return redirect(url_for("admin.admin"))
         filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], filename)
         f.save(filepath)
-        task = add_drugbank_task.apply_async([filepath, form_drugbank.version.data])
+        task = add_drugbank_task.apply_async([filepath, version])
         flash('Job {} created for "{}"'.format(task.task_id, f.filename))
         return redirect(url_for("admin.admin"))
 
     form_obo = StructuredOboForm()
     if form_obo.submit3.data and form_obo.validate():
         f = form_obo.obo_file.data
+        obo_type = dict(form_obo.obo_type.choices).get(form_obo.obo_type.data)
+        version = form_obo.version.data
+        if version in [resource["version"] for resource in structured_status
+                       if resource["type"] == obo_type]:
+            flash('OBO of type {} with version {} already in graph'.format(obo_type,
+                                                                           version))
+            return redirect(url_for("admin.admin"))
         filename = secure_filename(f.filename)
         filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], filename)
         f.save(filepath)
-        obo_type = dict(form_obo.obo_type.choices).get(form_obo.obo_type.data)
-        task = add_obo_task.apply_async([filepath, obo_type, form_obo.version.data])
+        task = add_obo_task.apply_async([filepath, obo_type, version])
         flash('Job {} created for "{}"'.format(task.task_id, f.filename))
         return redirect(url_for("admin.admin"))
 
