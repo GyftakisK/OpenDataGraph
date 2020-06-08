1dd43275500cbaf513e374fe759dfc3fc8053433
GyftakisK <gyftakiskon@gmail.com>
2020-06-08 11:56:52 +0300

    Add CLI to create DB

--- disease_graph_run_app.py
+++ disease_graph_run_app.py
@@ -7,3 +7,13 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
+
+
+@app.cli.command("init_db")
+def init_db():
+    db.create_all()
+    for admin_user in app.config['ADMINS']:
+        user = User(username=admin_user[:admin_user.index("@")], email=admin_user, is_admin=True)
+        user.set_password('admin')
+        db.session.add(user)
+    db.session.commit()
