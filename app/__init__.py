0123de50c712bd21f25a1fd450bbb8b506071e04
GyftakisK <gyftakiskon@gmail.com>
2020-06-20 18:53:07 +0300

    Remove unused migrate functionality

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
@@ -15,7 +14,6 @@ from knowledge_extractor.knowledge_extractor import KnowledgeExtractor
 
 
 db = SQLAlchemy()
-migrate = Migrate()
 login = LoginManager()
 login.login_view = 'admin.login'
 bootstrap = Bootstrap()
@@ -32,7 +30,6 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
